a25e7146b74861702ea163e394133cdd078bb1ca
Silvio Glöckner <silvio.gloeckner@outlook.de>
2024-08-12 17:19:27 +0200

    added ToDo class

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,19 @@
 "use client"
 
 import { useState } from 'react';
+import { ToDo } from './todo';
 
 export default function Page() {
-  const [toDo, setToDo] = useState('');
-  const [toDos, setToDos] = useState<string[]>([]);
+  const [toDo, setToDo] = useState<ToDo>(new ToDo());
+  const [toDos, setToDos] = useState<ToDo[]>([]);
 
   return (
     <main>
       <h1>Hello World!</h1>
-      <input className='border-2' onChange={e => setToDo(e.target.value)} />
+      <input className='border-2' onChange={e => setToDo({ todo: e.target.value })} />
       <button onClick={() => { setToDos([...toDos, toDo]) }}>Add ToDo</button>
       <ul>{
-        toDos.map(toDo => <li>{toDo}</li>)}</ul>
+        toDos.map(toDo => <li>{toDo.todo}</li>)}</ul>
     </main>
   );
 }
---
+++ src/app/todo.ts
@@ -0,0 +1,7 @@
+export class ToDo {
+    todo: string
+
+    constructor() {
+        this.todo = "";
+    }
+}
